bcc4a18cc4c55b09b69c90b0e29a5b1e0d809508
Nick Palmer <npalmer2267@sdsu.edu>
2025-03-26 18:41:11 -0700

    chore: update TypeScript configuration and remove unused client file

--- web/src/amplify/client.ts
+++
@@ -1,5 +0,0 @@
-// src/amplify/client.ts - This file generates the client for the Amplify project.
-import { generateClient } from 'aws-amplify/data';
-import type { Schema } from '../../amplify/data/resource';
-
-export const client = generateClient<Schema>();
